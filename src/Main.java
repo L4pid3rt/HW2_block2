67126e0c4d18af565327d9e7e9f7c45f78082e9c
L4pid3rt <dl4pt3v@yandex.ru>
2020-05-27 22:45:27 +0400

    HW 2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,47 @@
 public class Main {
+    final static byte ARRAYBOUNDARY = 4;
+
+    static String[][] strings = {
+            {"123", "12", "234"},
+            {"156", "645", "876", "0"},
+            {"8", "8", "7", "0"},
+            {"984", "98", "1", "5"}
+    };
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(arraySum(strings));
+        } catch (MyArraySizeException e) {
+            System.out.println("Wrong boundaries. String №" + e.getStringNumber() + " length " + e.getColumnsCount() +
+                    " strings " + e.getStringsCount());
+            e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            System.out.println("Wrong data type in string " + (e.getStringNumber() + 1) + " column " + (e.getColumnNumber() + 1));
+            e.printStackTrace();
+        }
+    }
+
+    static int arraySum(String[][] s) throws MyArraySizeException, MyArrayDataException {
+        int x = 0;
+        if (s.length < ARRAYBOUNDARY) {
+            throw new MyArraySizeException("Array boundaries exceeded", s.length, s[0].length, 1);
+        }
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].length < ARRAYBOUNDARY) {
+                throw new MyArraySizeException("Array boundaries exceeded", s.length, s[0].length, i + 1);
+            }
+        }
+        for (int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s[i].length; j++) {
+                try {
+                    x += Integer.parseInt(s[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Wrong data", i, j);
+                }
+
+            }
+        }
+        return x;
+    }
+
 }
